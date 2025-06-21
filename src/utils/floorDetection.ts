16d04a43dd1dd0448030699b5f9972d78e4bdbd2
Atootikhos <atootikhos@gmail.com>
2025-06-21 14:26:41 +1000

    Update floorDetection.ts

--- src/utils/floorDetection.ts
+++ src/utils/floorDetection.ts
@@ -3,7 +3,7 @@ export const detectFloor = async (imageFile: File): Promise<string> => {
   formData.append('image', imageFile);
 
   try {
-    const response = await fetch('https://visbackend.fly.dev/api/detect', {
+    const response = await fetch('https://vis-worker-backend.atootikhos.workers.dev/api/detect', {
       method: 'POST',
       body: formData,
     });
